0f9f28589af78dd08644b175b51eb5d812e997cd
ChristianAdamPalludan <madapal@hotmail.com>
2014-10-08 14:25:45 +0200

    Added more stuff

---
+++ src/flowteam/Load_and_Save.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package load_and_save;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Adam
+ */
+public class Load_and_Save {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        String p1 = "Bob,6,16,8,9";
+        String p2 = "Mona,3,7,1,16";
+        String p3 = "Lisa,14,5,8,3";
+        String p4 = "Jack,3,3,7,13";
+        String p5 = "Michael,5,3,16,3";
+        String p6 = "Richard,14,7,10,8";
+        Person persA = new Person("Morgan",3,6,10,13);
+        
+        ArrayList<String> personList = new ArrayList<String>();
+        
+        personList.add(p1);
+        personList.add(p2);
+        personList.add(p3);
+        personList.add(p4);
+        personList.add(p5);
+        personList.add(p6);
+        personList.add(persA.toString());
+        
+        FileHandlerStat.savePersons(personList, "people.txt");
+        
+        
+        ArrayList<Person> listOfPeople = new ArrayList<Person>();
+        ArrayList<String> fromTheDeadList = new ArrayList<String>();
+        fromTheDeadList = FileHandlerStat.load("people.txt");
+        if(fromTheDeadList == null){
+            System.out.println("Something went wrong... load returned null");   
+        }
+        else{
+            for(String peopleString : fromTheDeadList){
+                System.out.println(peopleString);
+                Person tempPerson = new Person( peopleString);
+                listOfPeople.add(tempPerson);
+                System.out.println(peopleString);
+            }
+        }
+        System.out.println("Now printing my person object");
+        for(Person onePerson : listOfPeople){
+            System.out.println("Person: " + onePerson.toString());
+            
+        }
+    }
+    
+}
---
+++ src/flowteam/Person.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package load_and_save;
+
+/**
+ *
+ * @author Adam
+ */
+
+
+public class Person {
+    private String name;
+    private int admin;
+    private int finisher;
+    private int analyzer;
+    private int creator;
+
+    public Person(String name, int admin, int finisher, int analyzer, int creator) {
+        this.name = name;
+        this.admin = admin;
+        this.finisher = finisher;
+        this.analyzer = analyzer;
+        this.creator = creator;
+    }
+
+    
+     public String toString(){
+    return name + "," + admin + "," + finisher + "," + analyzer + "," + creator;
+}
+    
+     public Person(String specialPeopleString){
+         String[] splitted = specialPeopleString.split(",");
+         name = splitted[0];
+         admin = Integer.parseInt(splitted[1]);
+         finisher = Integer.parseInt(splitted[2]);
+         analyzer = Integer.parseInt(splitted[3]);
+         creator = Integer.parseInt(splitted[4]);
+     }
+     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(int admin) {
+        this.admin = admin;
+    }
+
+    public int getFinisher() {
+        return finisher;
+    }
+
+    public void setFinisher(int finisher) {
+        this.finisher = finisher;
+    }
+
+    public int getAnalyzer() {
+        return analyzer;
+    }
+
+    public void setAnalyzer(int analyzer) {
+        this.analyzer = analyzer;
+    }
+
+    public int getCreator() {
+        return creator;
+    }
+
+    public void setCreator(int creator) {
+        this.creator = creator;
+    }
+    
+    
+   
+}
